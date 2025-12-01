23eb9437266c4c1418549210f17a0fb2ad498911
jahanzai6 <78846221+Jahanzaib320@users.noreply.github.com>
2025-12-01 15:23:16 +1000

    Add Toy Robot solution implementation

---
+++ toy-robot-magentus/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ToyRobotApp } from './components/ToyRobotApp'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToyRobotApp />
+  </React.StrictMode>
+)
